3566a5bb8d29818e8f567d0505c32ee47559a622
AlbaSM04 <alba.simon.mendez@gmail.com>
2024-11-07 00:56:45 +0100

    555

--- Assets/Scripts/Bola.cs
+++ Assets/Scripts/Bola.cs
@@ -1,42 +1,49 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class Bola : MonoBehaviour
 {
-    [SerializeField]Vector3 direccion=new Vector3 (0,0,0);
-    [SerializeField] float velocidad, fuerzaSalto, fuerzaX;
-    private float h;
-    private float v;
+    [Header("Movimiento")]
+    [SerializeField] float velocidad = 10f; 
+    private float h; 
+    private float v; 
 
-    Rigidbody rb;
+    [Header("Salto")]
+    [SerializeField] float fuerzaSalto = 5f; 
+    [SerializeField] float distanciaDeteccionSuelo = 1.1f; 
+    [SerializeField] LayerMask queEsSuelo; 
+
+    private Rigidbody rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    
     void Update()
     {
-        h = Input.GetAxisRaw("horizontal");
-        v = Input.GetAxisRaw("vertical");
-       
-       
+        h = Input.GetAxisRaw("Horizontal"); // h = 1 (D o →), h = -1 (A o ←), h = 0 (sin entrada)
+        v = Input.GetAxisRaw("Vertical");   // v = 1 (W o ↑), v = -1 (S o ↓), v = 0 (sin entrada)
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(0, fuerzaSalto, 0, ForceMode.Impulse);
+            if (DetectarSuelo())
+            {
+                rb.AddForce(0, fuerzaSalto, 0, ForceMode.Impulse);
+            }
         }
     }
-    private void FixedUpdate()
+
+    void FixedUpdate()
     {
-        rb.AddForce (h, 0, v, ForceMode.Force);
+        rb.AddForce(new Vector3(h, 0, v) * velocidad, ForceMode.Force);
     }
 
-    private void OnTriggerEnter(Collider other)
+    bool DetectarSuelo()
     {
-        Destroy (other.gameObject);
+        return Physics.Raycast(transform.position, Vector3.down, distanciaDeteccionSuelo, queEsSuelo);
     }
 }
+    
