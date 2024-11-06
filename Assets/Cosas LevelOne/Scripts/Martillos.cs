9f3d98ea2b55b3d881016b920378a364a2d0d5e9
AlbaSM04 <alba.simon.mendez@gmail.com>
2024-11-06 21:27:32 +0100

    06/11/24

---
+++ Assets/Cosas LevelOne/Scripts/Martillos.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Martillo : MonoBehaviour
+{
+    [Header("Oscilación")]
+    [SerializeField] float amplitud = 45f; 
+    [SerializeField] float frecuencia = 1f; 
+    [SerializeField] float anguloInicial = 1f; 
+    [SerializeField] float direccionOscilacion = 1f; 
+ 
+
+    private float tiempo;
+
+    void Update()
+    {
+        
+        tiempo += Time.deltaTime;
+        float anguloOscilacion = Mathf.Sin(tiempo * frecuencia) * amplitud;
+
+        
+        transform.localRotation = Quaternion.Euler(0, anguloInicial, direccionOscilacion * anguloOscilacion);
+    }
+}
--- Assets/Cosas LevelOne/Scripts/Sierra.cs
+++ Assets/Cosas LevelOne/Scripts/Sierra.cs
@@ -6,31 +6,31 @@ using UnityEngine;
 public class Sierra : MonoBehaviour
 {
     [Header("Movimiento")]
-    [SerializeField] Vector3 direccion; // Dirección del movimiento
-    [SerializeField] int velocidad = 6; // Velocidad de movimiento
+    [SerializeField] Vector3 direccion; 
+    [SerializeField] int velocidad = 6; 
 
     [Header("Rotación")]
-    [SerializeField] float velocidadGiro = 100f; // Velocidad de rotación
-    [SerializeField] Vector3 direccionGiro; // Eje de rotación
+    [SerializeField] float velocidadGiro = 100f; 
+    [SerializeField] Vector3 direccionGiro; 
 
     float tiempo;
-    float cambioDireccionIntervalo = 0.75f; // Intervalo de tiempo para cambiar de dirección
+    float cambioDireccionIntervalo = 0.75f; 
 
     void Update()
     {
-        // Rotación continua sobre el eje especificado
+        
         transform.Rotate(direccionGiro.normalized * velocidadGiro * Time.deltaTime);
 
-        // Movimiento entre dos puntos
+        
         tiempo += Time.deltaTime;
         if (tiempo >= cambioDireccionIntervalo)
         {
-            // Cambia la dirección y reinicia el temporizador
+            
             direccion = -direccion;
             tiempo = 0f;
         }
 
-        // Mueve la sierra en la dirección actual
+        
         transform.Translate(direccion.normalized * velocidad * Time.deltaTime, Space.World);
     }
 }
